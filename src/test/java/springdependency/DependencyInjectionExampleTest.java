1f3870cd70435e2a69788c52e36b85c220ead4ea
LakshayS <lakshay13@gmail.com>
2017-04-09 23:03:52 +0200

    Committing SimpleTest after changing the project directory structure

--- src/test/java/springdependency/DependencyInjectionExampleTest.java
+++ src/test/java/springdependency/DependencyInjectionExampleTest.java
@@ -1,9 +1,10 @@
+package springdependency;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import springdependency.Addition;
 
 /**
  * @RunWith: This basically kind of instantiates the spring and it needs an application context to be instantiated.
